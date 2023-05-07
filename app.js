64e563ea2bbd076123d0f468b8051af366e2d106
willyfy <fywilly@gmail.com>
2023-05-07 12:39:51 +0100

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!Sagnol how are you. So you learn how to code with git'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
